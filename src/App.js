f7476356f71e0e9744ed807c11e0f10278348746
DSPsai <66173636+DSPsai@users.noreply.github.com>
2022-11-16 11:57:10 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Home from "./Home";
+import "./metabutton.css";
+import './App.css'
+function isMobileDevice() {
+  return window.innerWidth <= 600 ? true : false;
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+async function connect(onConnected) {
+  if (!window.ethereum) {
+    alert("Get MetaMask!");
+    return;
+  }
+
+  const accounts = await window.ethereum.request({
+    method: "eth_requestAccounts",
+  });
+
+  onConnected(accounts[0]);
+}
+
+async function checkIfWalletIsConnected(onConnected) {
+  if (window.ethereum) {
+    const accounts = await window.ethereum.request({
+      method: "eth_accounts",
+    });
+
+    if (accounts.length > 0) {
+      const account = accounts[0];
+      onConnected(account);
+      return;
+    }
+
+    if (isMobileDevice()) {
+      await connect(onConnected);
+    }
+  }
+}
+
+
+export default function MetaMaskAuth({ onAddressChanged }) {
+  const [userAddress, setUserAddress] = useState("");
+
+  useEffect(() => {
+    checkIfWalletIsConnected(setUserAddress);
+  }, []);
+
+  useEffect(() => {
+    onAddressChanged(userAddress);
+  }, [userAddress]);
+
+  return <><div className="App-header">{userAddress ? (
+    <div>
+      <Home userAddress={userAddress} />
+      {/* Connected with <Address userAddress={userAddress} /> */}
     </div>
+  ) : (
+    <Connect setUserAddress={setUserAddress} />
+  )}</div></>
+}
+
+
+function Connect({ setUserAddress }) {
+  if (isMobileDevice()) {
+    const dappUrl = "metamask-auth.ilamanov.repl.co"; // TODO enter your dapp URL. For example: https://uniswap.exchange. (don't enter the "https://")
+    const metamaskAppDeepLink = "https://metamask.app.link/dapp/" + dappUrl;
+    return (
+      <a href={metamaskAppDeepLink}>
+        <button className={'login-button'}>
+          Connect to MetaMask
+        </button>
+      </a>
+    );
+  }
+
+
+  return (
+    <button className={'login-button'} onClick={() => connect(setUserAddress)}>
+      Connect to MetaMask
+    </button>
   );
 }
 
-export default App;
+
+function Address({ userAddress }) {
+  return (
+    <span className={'address'}>{userAddress.substring(0, 5)}…{userAddress.substring(userAddress.length - 4)}</span>
+  );
+}
